b7918c82065b16e63b3566017df1ef28f494a703
noahark <dcsun57@gmail.com>
2023-11-14 01:05:58 +0900

    로직 완료 선 추가 버그 잡기

--- lib/page/secondePageProblem.dart
+++ lib/page/secondePageProblem.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+import 'package:music_notes/music_notes.dart';
+
 
 class SecondePageProblem extends StatefulWidget {
   const SecondePageProblem({super.key});
@@ -16,25 +18,26 @@ class _SecondePageProblemState extends State<SecondePageProblem> {
   // after
   List<List<dynamic>> note_height_list =
   [
-    [4.0,0],
-    [17.0,1],
-    [31.0,2],
-    [42.0,3],
-    [55.0,4],
-    [67.5,5],
-    [81.0,6],
-    [94.5,7],
-    [108.0,8],
-    [121.5,9],
-    [135.0,10],
-    [145.0,11],
-    [159.0,12],
-    [172.0,13],
-    [184.0,14],
+    [4.0,0,Note.b.inOctave(5)],
+    [17.0,1,Note.a.inOctave(5)],
+    [31.0,2,Note.g.inOctave(5)],
+    [42.0,3,Note.f.inOctave(5)],
+    [55.0,4,Note.e.inOctave(5)],
+    [67.5,5,Note.d.inOctave(5)],
+    [81.0,6,Note.c.inOctave(5)],
+    [94.5,7,Note.b.inOctave(4)],
+    [108.0,8,Note.a.inOctave(4)],
+    [121.5,9,Note.g.inOctave(4)],
+    [135.0,10,Note.f.inOctave(4)],
+    [145.0,11,Note.e.inOctave(4)],
+    [159.0,12,Note.d.inOctave(4)],
+    [172.0,13,Note.c.inOctave(4)],
+    [184.0,14,Note.b.inOctave(3)],
   ];
   // List<double> note_height_list = [31,42,55,67.5,81,94.5,108,121.5];
   late List<double> randomItems ;
   late List<int> randomNoteNumber ;
+  late List<dynamic> randomNote ;
   double downNoteLeft = 180.0;
 
   bool setEquals<T>(Set<T>? a, Set<T>? b) {
@@ -126,7 +129,99 @@ class _SecondePageProblemState extends State<SecondePageProblem> {
     }
   }
 
-  int? intervalNumber = null;
+  String? intervalNumber = null;
+
+  Widget showIntervalNameBefore(String intervalNumber){
+    return SizedBox(
+      child: Column(
+        children: [
+          Text('음정 이름을 고르세요'),
+          const SizedBox(height: 10.0,),
+          SizedBox(
+            height: 30.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'd' + intervalNumber;
+                  });
+                }, child: Text
+                  ('d' + intervalNumber)),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'P' + intervalNumber;
+                  });
+                }, child: Text('P'+intervalNumber)),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'A' + intervalNumber;
+                  });
+                }, child: Text('A'+intervalNumber)),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10.0,),
+          SizedBox(
+            height: 30.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'dd' + intervalNumber;
+                  });
+                }, child: Text('dd'+intervalNumber)),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'm' + intervalNumber;
+                  });
+                }, child: Text('m'+intervalNumber)),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'M' + intervalNumber;
+                  });
+                }, child: Text('M'+intervalNumber)),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    answerInterval = 'AA' + intervalNumber;
+                  });
+                }, child: Text('AA'+intervalNumber)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget showIntervalName(String? intervalNumber){
+    if (intervalNumber == null){
+      return SizedBox(height: 100,);
+    } else {
+      return showIntervalNameBefore(intervalNumber);
+    }
+  }
+
+  String? answerInterval = null;
+
+  Widget showAnswer(String? answerInterval){
+    if (answerInterval == null){
+      return SizedBox(height: 10.0,);
+    } else {
+      return showAnswerBefore(answerInterval);
+    }
+  }
+
+  Widget showAnswerBefore(String answerInterval){
+    if (answerInterval == randomNote[0].interval(randomNote[1]).toString()){
+      print(answerInterval);
+      return Text('정답입니다.');
+    } else {
+      print(answerInterval);
+      return Text('오답입니다.');
+    }
+  }
 
   @override
   void initState() {
@@ -138,9 +233,13 @@ class _SecondePageProblemState extends State<SecondePageProblem> {
     randomItems.sort();
     randomNoteNumber = [note_height_list[0][1],note_height_list[1][1]];
     randomNoteNumber.sort();
+    randomNote = [note_height_list[0][2],note_height_list[1][2]];
+    randomNote.sort();
 
     print(randomItems);
     print(randomNoteNumber);
+    print(randomNote);
+    print(randomNote[0].interval(randomNote[1]).toString());
 
     if ((note_height_list[0][1]-note_height_list[1][1]).abs()==1){
       downNoteLeft = 207;
@@ -196,73 +295,127 @@ class _SecondePageProblemState extends State<SecondePageProblem> {
               addLineDown(randomNoteNumber[0],randomNoteNumber[1]),
             ]
           ),
-          ElevatedButton(onPressed: (){
-
-            // 새로운 문제 생성
-            note_height_list.shuffle();
-            // 이전 문제와 동일하지 않게 방지 하는 장치
-            // 동일하면 while문이 계속 실행
-            print((note_height_list[0][1]-note_height_list[1][1]).abs());
-            while (
-            // 이전 문제와 동일하지 않게 방지 하는 장치
-            setEquals(
-                [note_height_list[0][0],note_height_list[1][0]].toSet(),
-                [randomItems[0],randomItems[1]].toSet())
-            // 7넘게 차이나면 다시 생성
-            |((note_height_list[0][1]-note_height_list[1][1]).abs()>7)
-            ){
-              note_height_list.shuffle();
-            }
-
-            setState(() {
-              // 문제 적용
-              randomItems = [note_height_list[0][0],note_height_list[1][0]];
-              randomItems.sort();
-              randomNoteNumber = [note_height_list[0][1],note_height_list[1][1]];
-              randomNoteNumber.sort();
-            });
-
-            print((note_height_list[0][1]-note_height_list[1][1]).abs());
-            if ((note_height_list[0][1]-note_height_list[1][1]).abs()==1){
-              setState(() {
-                downNoteLeft = 207;
-              });
-            } else {
-              setState(() {
-                downNoteLeft = 180;
-              });
-            }
-
-            print(randomItems);
-            print(randomNoteNumber);
-
-            // List randomItems = note_height_list.take(2).toList();
-          }, child: const Text('버튼')),
+          const SizedBox(height: 10.0,),
+          Text('음정 번호를 쓰세요'),
           const SizedBox(height: 10.0,),
           SizedBox(
-            height: 30.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
               children: [
-                ElevatedButton(onPressed: (){}, child: const Text('1')),
-                ElevatedButton(onPressed: (){}, child: const Text('2')),
-                ElevatedButton(onPressed: (){}, child: const Text('3')),
-                ElevatedButton(onPressed: (){}, child: const Text('4')),
+                SizedBox(
+                  height: 30.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '1';
+                        });
+                      }, child: const Text('1')),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '2';
+                        });
+                      }, child: const Text('2')),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '3';
+                        });
+                      }, child: const Text('3')),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '4';
+                        });
+                      }, child: const Text('4')),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10.0,),
+                SizedBox(
+                  height: 30.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '5';
+                        });
+                      }, child: const Text('5')),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '6';
+                        });
+                      }, child: const Text('6')),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '7';
+                        });
+                      }, child: const Text('7')),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          intervalNumber = '8';
+                        });
+                      }, child: const Text('8')),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          const SizedBox(height: 10.0,),
-          SizedBox(
-            height: 30.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(onPressed: (){}, child: const Text('5')),
-                ElevatedButton(onPressed: (){}, child: const Text('6')),
-                ElevatedButton(onPressed: (){}, child: const Text('7')),
-                ElevatedButton(onPressed: (){}, child: const Text('8')),
-              ],
-            ),
+          SizedBox(height: 30.0,),
+          showIntervalName(intervalNumber),
+          SizedBox(height: 30,),
+          showAnswer(answerInterval),
+          ElevatedButton(
+              onPressed: (){
+
+                // 새로운 문제 생성
+                note_height_list.shuffle();
+                // 이전 문제와 동일하지 않게 방지 하는 장치
+                // 동일하면 while문이 계속 실행
+                print((note_height_list[0][1]-note_height_list[1][1]).abs());
+                while (
+                // 이전 문제와 동일하지 않게 방지 하는 장치
+                setEquals(
+                    [note_height_list[0][0],note_height_list[1][0]].toSet(),
+                    [randomItems[0],randomItems[1]].toSet())
+                // 7넘게 차이나면 다시 생성
+                |((note_height_list[0][1]-note_height_list[1][1]).abs()>7)
+                ){
+                  note_height_list.shuffle();
+                }
+
+                setState(() {
+                  // 문제 적용
+                  randomItems = [note_height_list[0][0],note_height_list[1][0]];
+                  randomItems.sort();
+                  randomNoteNumber = [note_height_list[0][1],note_height_list[1][1]];
+                  randomNoteNumber.sort();
+
+                  answerInterval = null;
+                  intervalNumber = null;
+                });
+
+                print((note_height_list[0][1]-note_height_list[1][1]).abs());
+                if ((note_height_list[0][1]-note_height_list[1][1]).abs()==1){
+                  setState(() {
+                    downNoteLeft = 207;
+                  });
+                } else {
+                  setState(() {
+                    downNoteLeft = 180;
+                  });
+                }
+
+                randomNote = [note_height_list[0][2],note_height_list[1][2]];
+                randomNote.sort();
+
+                print(randomItems);
+                print(randomNoteNumber);
+                print(randomNote);
+                print(randomNote[0].interval(randomNote[1]));
+
+                // List randomItems = note_height_list.take(2).toList();
+              }, child: const Text('다음문제')
           ),
         ],
       ),
